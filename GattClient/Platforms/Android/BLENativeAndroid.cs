7d08a44e0dba5ecde4112145bef59e7d56b59781
らうしぶ <75715137+Lausiv1024@users.noreply.github.com>
2025-01-06 20:34:12 +0900

    BLENativeManager.CurrentよりAdapterを取得可能に

--- GattClient/Platforms/Android/BLENativeAndroid.cs
+++ GattClient/Platforms/Android/BLENativeAndroid.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Android.App;
+using Android.Bluetooth;
 using Android.Bluetooth.LE;
 using Android.Content;
 using Android.OS;
@@ -13,14 +14,43 @@ namespace GattClient
 {
     public class BLENativeAndroid
     {
+
+        public BluetoothAdapter BluetoothAdapter { get; }
+
         public BLENativeAndroid(Guid serviceId, Guid characteristicId)
         {
             
         }
     }
 
+    /// <summary>
+    /// ここにBluetoothAdapterを格納する。
+    /// </summary>
+    public class BLENativeManager
+    {
+        public static BLENativeManager Current { get {return c; } }
+        private static BLENativeManager c;
+        public BluetoothAdapter BluetoothAdapter { get; }
+        public BLENativeManager(BluetoothAdapter adapter){
+
+            BluetoothAdapter = adapter;
+            if (c != null) //2回目の呼び出しはとりあえず許可しない
+            {
+                throw new InvalidOperationException("Current is Already Initialized");
+            }
+            c = this;
+        }
+    }
+
     public class BLEServiceScannerNativeAndroid
     {
-        
+        public BLEServiceScannerNativeAndroid()
+        {
+            
+        }
+        private void scanLeDevice()
+        {
+
+        }
     }
 }
--- GattClient/Platforms/Android/MainActivity.cs
+++ GattClient/Platforms/Android/MainActivity.cs
@@ -1,4 +1,6 @@
 ﻿using Android.App;
+using Android.Bluetooth;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 
@@ -7,5 +9,11 @@ namespace GattClient
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        public override void OnCreate(Bundle? savedInstanceState, PersistableBundle? persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            BluetoothManager manager = (BluetoothManager)GetSystemService(Context.BluetoothService) ?? throw new InvalidOperationException("Bluetooth Manager is null!");
+            new BLENativeManager(manager.Adapter);
+        }
     }
 }
