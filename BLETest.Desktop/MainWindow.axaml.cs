cf550c09d43596a1653549004b9b6df761920c14
stig-enhanced-goto <75715137+Lausiv1024@users.noreply.github.com>
2025-11-30 18:28:06 +0900

    Mainwindowの実装を追加

--- BLETest.Desktop/MainWindow.axaml.cs
+++ BLETest.Desktop/MainWindow.axaml.cs
@@ -1,24 +1,98 @@
 using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using FluentAvalonia.UI.Controls;
+using System.Threading.Tasks;
+using BLETest.Settings;
+using Avalonia.Threading;
 
 namespace BLETest.Desktop
 {
     public partial class MainWindow : Window
     {
+        public byte cnt = 0;
+        public const int BufferSize = 1024;
+        BLECommunicationServer? _bleCommunicationServer;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void SendVal_KeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
+        public async Task BleMain()
         {
+            _bleCommunicationServer = new BLECommunicationServer(
+                BLESettings.ServiceId,
+                BLESettings.WriteCharacteristic,
+                BLESettings.NotifyCharacteristic,
+                "BLETest");
+            _bleCommunicationServer.OnDataReceived += OndataReceived;
+            await _bleCommunicationServer.BLEInitializeAsync();
         }
 
-        private void NotifyBut_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        private void OndataReceived(object? sender, OnDataReceivedEventArgs e)
         {
+            if (e.Data.Length >= 2 && e.Data[0] == 0xFF)
+            {
+                if (e.Data[1] == 0x80)
+                {
+                    //Dispatcher.InvokeAsync(() => NotifyBut.Content = "1");
+                    KeyControl.KeyDown(0x31); // '1' key down
+                } else if (e.Data[1] == 0x81)
+                {
+                    //Dispatcher.InvokeAsync(() => NotifyBut.Content = "0");
+                    KeyControl.KeyUp(0x31); // '1' key up
+                }
+                return;
+            }
+            string str = System.Text.Encoding.UTF8.GetString(e.Data);
+            Dispatcher.UIThread.Post(() => ReceivedVal.Text += $"[{e.DeviceId}] : {str}\n");
         }
 
-        private void NewDeviceMenu_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        private async Task Notify(string message)
         {
+            if (_bleCommunicationServer != null)
+            {
+                byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
+                await _bleCommunicationServer.NotifyAsync(data);
+            }
+        }
+
+        private async void SendVal_KeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
+        {
+            if (e.Key == Avalonia.Input.Key.Enter)
+            {
+                if (!string.IsNullOrEmpty(SendVal.Text))
+                {
+                    await Notify(SendVal.Text);
+                    //SendVal.Text = "";
+                }
+            }
+        }
+
+        private async void NotifyBut_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(SendVal.Text))
+            {
+                await Notify(SendVal.Text);
+            }
+        }
+
+        private async void NewDeviceMenu_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            using var qrGenerator = new QRCoder.QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode($"TEST_DEVICE_ID,TEST_PUBLIC_KEY", QRCoder.QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCoder.PngByteQRCode(qrCodeData);
+
+            using var ms = new System.IO.MemoryStream(qrCode.GetGraphic(20));
+            QRImage.Source = new Bitmap(ms);
+            await AddDevice.ShowAsync();
+            
+        }
+
+        private async void Window_Initialized(object? sender, System.EventArgs e)
+        {
+            await BleMain();
         }
     }
 }
